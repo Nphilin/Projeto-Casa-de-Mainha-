daa595c0b9dc6c9cad41763694359ddeaa9a300b
Nphilin <pedrogluna@hotmail.com.br>
2026-05-09 16:31:12 -0300

    Primeiro commit da entrutura do projeto, mais alterações em breve

---
+++ casa-de-mainha/src/main/java/com/example/casa_de_mainha/CasaDeMainhaApplication.java
@@ -0,0 +1,13 @@
+package com.example.casa_de_mainha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasaDeMainhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasaDeMainhaApplication.class, args);
+	}
+
+}
