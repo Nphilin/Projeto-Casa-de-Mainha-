976f5467911bc82baa3d72b117b5e3cd8a3fa538
Abelardo <vanzoff.ads@gmail.com>
2026-05-25 16:30:39 -0300

    add: adicionado o crud no ConsumoRepository

--- casa-de-mainha/src/main/java/com/example/casa_de_mainha/Repository/ConsumoRepository.java
+++ casa-de-mainha/src/main/java/com/example/casa_de_mainha/Repository/ConsumoRepository.java
@@ -1,12 +1,12 @@
 package com.example.casa_de_mainha.Repository;
 
 import com.example.casa_de_mainha.Entity.Consumo;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.List;
+import org.springframework.data.repository.CrudRepository;
 
 @Repository
-public interface ConsumoRepository extends JpaRepository<Consumo, Long> {
+public interface ConsumoRepository extends CrudRepository<Consumo, Long> {
 
     // Busca todos os gastos extras de uma reserva específica
     List<Consumo> findByReservaId(Long reservaId);
