36cb6b58caf11dd396bbcae64c509ba5b15b46f0
Nphilin <pedrogluna@hotmail.com.br>
2026-06-01 14:55:03 -0300

    Fase de testes automaticos

--- casa-de-mainha/src/main/java/com/example/casa_de_mainha/Repository/UsuariosRepository.java
+++ casa-de-mainha/src/main/java/com/example/casa_de_mainha/Repository/UsuariosRepository.java
@@ -1,20 +1,17 @@
 package com.example.casa_de_mainha.Repository;
 
-import com.example.casa_de_mainha.Entity.Usuarios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
-
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.casa_de_mainha.Entity.Usuarios;
 
 @Repository
 public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
 
-    List<Usuarios> listarLoginContainingIgnoreCase(String login);
+    List<Usuarios> findByLoginContainingIgnoreCase(String login);
 
     Optional<Usuarios> findByLoginIgnoreCase(String login);
 
