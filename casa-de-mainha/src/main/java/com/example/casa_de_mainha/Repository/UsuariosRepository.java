3575b02062062d0fef5487b2519e49379dd1fd17
Maicão <maicongomes613@gmail.com>
2026-06-01 13:50:49 -0300

    fix: usuariosrepository login

--- casa-de-mainha/src/main/java/com/example/casa_de_mainha/Repository/UsuariosRepository.java
+++ casa-de-mainha/src/main/java/com/example/casa_de_mainha/Repository/UsuariosRepository.java
@@ -1,18 +1,23 @@
 package com.example.casa_de_mainha.Repository;
 
 import com.example.casa_de_mainha.Entity.Usuarios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface UsuariosRepository extends CrudRepository<Usuarios, Long> {
+public interface UsuariosRepository extends JpaRepository<Usuarios, Long> {
 
-    List<Usuarios> listarLoginContainingIgnoreCase(String login);
+    @Query("SELECT u FROM Usuarios u WHERE u.login = :login")
+    List<Usuarios> listarLogin(@Param("login") String login);
 
-    Optional<Usuarios> buscarporLoginIgnoreCase(String login);
+    Optional<Usuarios> findByLoginIgnoreCase(String login);
 
     boolean existsByLoginIgnoreCase(String login);
 
